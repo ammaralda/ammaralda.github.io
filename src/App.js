4121c6978b6d7f509e9a88afdeef1270bb5af6e1
Dehan <dehanseal@outlook.com>
2022-03-24 22:18:23 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+import { Layout } from "antd";
+
+import "./App.css";
+const { Footer } = Layout;
+
+function Header(props) {
+  return (
+    <div className="App-header"> 
+      It's Me Handiana
+    </div>
+    )
+
+}
+
+function Content() {
+return (
+<div className="App-header"> 
+      Content
+    </div>
+)
+}
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+      <Header />
+      <Content/>
+      <Footer>Footer</Footer>
+    </Layout>
   );
 }
 
